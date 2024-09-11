a6b04b8a597640f10519ff21d3766e42b1875f2a
Issam <issamhn2000@gmail.com>
2024-09-11 12:17:18 +0300

    Added declination

--- src/dmc_view/main.py
+++ src/dmc_view/main.py
@@ -21,23 +21,33 @@ def main():
 
     parser = ArgumentParser()
 
-    parser.add_argument('-a',
+    parser.add_argument(
+        '-a',
         help= 'direction measured in degrees clockwise from north',
         type= float,
         default=None,
         nargs='?',
         metavar='azimuth'
     )
+    parser.add_argument(
+        '-d',
+        help='difference between real north and magnetic north',
+        type=float, 
+        nargs='?',
+        default=None,
+        metavar='declination'
+    )
     
 
     args : Namespace = parser.parse_args()
 
     azimuth: float = args.a if args.a is not None else get_float_input("Enter the azimuth angle in degrees; for example 40.45",0.0) # azimuth
+    declination: float = args.d if args.d is not None else get_float_input("Enter the declination angle in degrees; for example 30.0", 0.0) # declination
 
     app = QApplication()
     compass = Compass()
     compass.show()
-    compass.update_declination(10)  #
+    compass.update_declination(declination)  #
     compass.update_angle(
         azimuth
     )  # This is Azimuth and can be float to two decimal places for example 35.55
