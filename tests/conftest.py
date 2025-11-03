f60ad2571cc87e294d22bd77719b49e42b0b6f40
iso <iso.techdev@gmail.com>
2025-11-03 15:28:45 +0200

    fixing ruff isort black prospector

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,14 @@
-import pytest
 import os
 
-def pytest_collection_modifyitems(config, items):
+import pytest
+
+
+def pytest_collection_modifyitems(_, items):
     """Skip ALL GUI tests in CI"""
     if os.getenv('CI'):
         skip_gui = pytest.mark.skip(reason="GUI tests skipped in CI")
         gui_files = ['test_acceleration', 'test_compass', 'test_simulator']
-        
+
         for item in items:
             if any(gui_file in item.nodeid for gui_file in gui_files):
                 item.add_marker(skip_gui)
